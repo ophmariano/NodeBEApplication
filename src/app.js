6a095bd6dc1897ea6f8dc953a09de7e627dc2097
Paulo Mariano <ophmariano@gmail.com>
2023-01-13 15:07:15 +0100

    Eslint fixes (back to work)

--- src/app.js
+++ src/app.js
@@ -25,6 +25,6 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
   const { id } = req.params;
   const contract = await Contract.findOne({ where: { id } });
   if (!contract) return res.status(404).end();
-  res.json(contract);
+  return res.json(contract);
 });
 module.exports = app;
--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -3,6 +3,6 @@ const getProfile = async (req, res, next) => {
   const profile = await Profile.findOne({ where: { id: req.get('profile_id') || 0 } });
   if (!profile) return res.status(401).end();
   req.profile = profile;
-  next();
+  return next();
 };
 module.exports = { getProfile };
